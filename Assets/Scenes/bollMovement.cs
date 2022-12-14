36002d092609a6e9054d1eb166be244cff52cc2f
ItsMeHappy <120007989+ItsMeHappy@users.noreply.github.com>
2022-12-14 09:23:44 +0100

    Update bollMovement.cs

--- Assets/Scenes/bollMovement.cs
+++ Assets/Scenes/bollMovement.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class bollMovement : MonoBehaviour
 {
+    public Rigidbody2D rb2d;
 
     public KeyCode right;
+
+    public KeyCode left;
+
+    public float force;
+
+    Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +25,12 @@ public class bollMovement : MonoBehaviour
     {
         if (Input.GetKey(right))
         {
+            rb2d.AddForce(Vector3.right * force * Time.deltaTime);
+        }
 
+        if (Input.GetKey(left))
+        {
+            rb2d.AddForce(Vector3.left * force * Time.deltaTime);
         }
     }
 }
