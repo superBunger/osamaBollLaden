4f832730bacccafdd5dd2adfa1fbee4abe865a4f
superBunger <120007693+superBunger@users.noreply.github.com>
2022-12-16 09:37:41 +0100

    kameran följer seplaren (lol!?!?!)

--- Assets/Scenes/bollMovement.cs
+++ Assets/Scenes/bollMovement.cs
@@ -16,13 +16,11 @@ public class bollMovement : MonoBehaviour
     public bool isGrounded;
     public bool isSlamming;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(right))
